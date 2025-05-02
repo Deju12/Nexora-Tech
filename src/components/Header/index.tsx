571913402607975fc27a4b159e7cbf360052d145
dejene tesfaye <dejenet@tnt.com>
2025-05-02 10:02:47 +0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/nexoraHeader.png"
+                  src="/images/logo/nexoralogo.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/nexoraHeader.png"
+                  src="/images/logo/nexoralogo.png"
                   alt="logo"
                   width={140}
                   height={30}
